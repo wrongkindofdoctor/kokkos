8eaf18b598c96bb00c83c7119a509222d1a5de1f
Christian Trott <crtrott@sandia.gov>
2018-05-17 18:02:37 -0600

    StdExecutors: Support parallel_reduce for sum through atomic reduction

--- core/src/StdExecutors/Kokkos_StdExecutors_Parallel.hpp
+++ core/src/StdExecutors/Kokkos_StdExecutors_Parallel.hpp
@@ -172,99 +172,105 @@ public:
 };
 
 // TODO finish this once execution space scratch allocation stuff is set up
-//template <typename FunctorType, typename Executor, typename ReducerType, typename... Traits>
-//class ParallelReduce<
-//  FunctorType,
-//  Kokkos::RangePolicy<Traits...>,
-//  ReducerType,
-//  Kokkos::Experimental::StdExecutors<Executor>
-//>
-//{
-//private:
-//  using execution_space_t = Kokkos::Experimental::StdExecutors<Executor>;
-//  using policy_t = Kokkos::RangePolicy<Traits...>;
-//  using work_range_t = typename policy_t::WorkRange;
-//  using member_t = typename policy_t::member_type;
-//  using work_tag_t = typename policy_t::work_tag;
-//
+template <typename FunctorType, typename Executor, typename ReducerType, typename... Traits>
+class ParallelReduce<
+  FunctorType,
+  Kokkos::RangePolicy<Traits...>,
+  ReducerType,
+  Kokkos::Experimental::StdExecutors<Executor>
+>
+{
+  private:
+    using execution_space_t = Kokkos::Experimental::StdExecutors<Executor>;
+    using policy_t = Kokkos::RangePolicy<Traits...>;
+    using work_range_t = typename policy_t::WorkRange;
+    using member_t = typename policy_t::member_type;
+    using work_tag_t = typename policy_t::work_tag;
+ 
+  typedef Kokkos::Impl::if_c< std::is_same<InvalidType,ReducerType>::value, FunctorType, ReducerType> ReducerConditional; 
 //  using ReducerConditional = std::conditional_t< std::is_same_v<InvalidType,ReducerType>, FunctorType, ReducerType>;
-//  using ReducerTypeFwd = typename ReducerConditional::type;
-//  using WorkTagFwd = typename std::conditional_t< std::is_same_v<InvalidType,ReducerType>, work_tag_t, void>::type;
-//  using Analysis = FunctorAnalysis<FunctorPatternInterface::REDUCE , policy_t, FunctorType>;
-//  using ValueInit = Kokkos::Impl::FunctorValueInit<ReducerTypeFwd, WorkTagFwd>;
-//  using ValueJoin = Kokkos::Impl::FunctorValueJoin<ReducerTypeFwd, WorkTagFwd>;
-//  using pointer_type = typename Analysis::pointer_type;
-//  using reference_type = typename Analysis::reference_type;
-//
-//  execution_space_t* m_instance;
-//  FunctorType const m_functor;
-//  policy_t const m_policy;
-//  ReducerType const m_reducer;
-//  pointer_type const m_result_ptr;
-//
-//
-//  template <typename Index>
-//  inline static void _invoke(FunctorType const& functor, Index idx, std::true_type) {
-//    functor(idx);
-//  }
-//
-//  template <typename Index>
-//  inline static void _invoke(FunctorType const& functor, Index idx, std::false_type) {
-//    functor(work_tag_t{}, idx);
-//  }
-//
-//
-//public:
-//
-//  template <typename ViewType>
-//  inline
-//  ParallelReduce(
-//    FunctorType const& arg_functor,
-//    policy_t const& arg_policy,
-//    ViewType const&  arg_view,
-//    typename std::enable_if<
-//      Kokkos::is_view< ViewType >::value &&
-//      !Kokkos::is_reducer_type<ReducerType>::value,
-//      void*
-//    >::type = NULL
-//  ) : m_instance(execution_space_t::s_default_instance.get()),
-//      m_functor(arg_functor),
-//      m_policy(arg_policy),
-//      m_reducer( InvalidType()),
-//      m_result_ptr(arg_view.data())
-//  { }
-//
-//  inline
-//  ParallelReduce(
-//    FunctorType const& arg_functor,
-//    policy_t const& arg_policy,
-//    ReducerType const& reducer
-//  ) : m_instance(execution_space_t::s_default_instance.get()),
-//      m_functor(arg_functor),
-//      m_policy(arg_policy),
-//      m_reducer(reducer),
-//      m_result_ptr(reducer.view().data())
-//  { }
-//
-//  inline void execute() const
-//  {
-//    // TODO Don't ignore schedule type?
-//    // TODO obey chunking?
-//    auto& impl = m_instance->m_impl;
-//    using future_type = typename Kokkos::Experimental::Impl::StdExecutorsImpl<Executor>::future_type;
-//    auto n = m_policy.end() - m_policy.begin();
-//
-//    reference_type update = ValueInit::init(
-//      ReducerConditional::select(m_functor , m_reducer),
-//    );
-//
-//    impl->fence_futures.emplace_back(std::make_unique<future_type>(
-//      impl->m_executor.bulk_twoway_execute([functor=m_functor,offset=m_policy.begin()](auto n, auto) {
-//        ParallelReduce::_invoke(functor, offset+n, update, std::is_same<work_tag_t, void>{});
-//      }, n, [](){}, [](){ return 0; })
-//    ));
-//  }
-//};
+  using ReducerTypeFwd = typename ReducerConditional::type;
+  using WorkTagFwd = typename std::conditional_t< std::is_same_v<InvalidType,ReducerType>, work_tag_t, void>;
+  using Analysis = FunctorAnalysis<FunctorPatternInterface::REDUCE , policy_t, FunctorType>;
+  using ValueInit = Kokkos::Impl::FunctorValueInit<ReducerTypeFwd, WorkTagFwd>;
+  using ValueJoin = Kokkos::Impl::FunctorValueJoin<ReducerTypeFwd, WorkTagFwd>;
+  using value_type = typename Analysis::value_type;
+  using pointer_type = typename Analysis::pointer_type;
+  using reference_type = typename Analysis::reference_type;
+
+  execution_space_t* m_instance;
+  FunctorType const m_functor;
+  policy_t const m_policy;
+  ReducerType const m_reducer;
+  pointer_type const m_result_ptr;
+
+
+  template <typename Index>
+  inline static void _invoke(FunctorType const& functor, Index idx, value_type& val, std::true_type) {
+    functor(idx,val);
+  }
+
+  template <typename Index>
+  inline static void _invoke(FunctorType const& functor, Index idx, value_type& val, std::false_type) {
+    functor(work_tag_t{}, idx,val);
+  }
+
+
+public:
+
+  template <typename ViewType>
+  inline
+  ParallelReduce(
+    FunctorType const& arg_functor,
+    policy_t const& arg_policy,
+    ViewType const&  arg_view,
+    typename std::enable_if<
+      Kokkos::is_view< ViewType >::value &&
+      !Kokkos::is_reducer_type<ReducerType>::value,
+      void*
+    >::type = NULL
+  ) : m_instance(execution_space_t::s_default_instance.get()),
+      m_functor(arg_functor),
+      m_policy(arg_policy),
+      m_reducer( InvalidType()),
+      m_result_ptr(arg_view.data())
+  { }
+
+  inline
+  ParallelReduce(
+    FunctorType const& arg_functor,
+    policy_t const& arg_policy,
+    ReducerType const& reducer
+  ) : m_instance(execution_space_t::s_default_instance.get()),
+      m_functor(arg_functor),
+      m_policy(arg_policy),
+      m_reducer(reducer),
+      m_result_ptr(reducer.view().data())
+  { }
+
+  inline void execute() const
+  {
+    // TODO Don't ignore schedule type?
+    // TODO obey chunking?
+    // TODO support something else than += reduction
+    auto& impl = m_instance->m_impl;
+    using future_type = std::experimental::execution::executor_future_t<Executor, std::atomic<value_type>>;//typename Kokkos::Experimental::Impl::StdExecutorsImpl<Executor>::future_type;
+    auto n = m_policy.end() - m_policy.begin();
+
+      std::atomic<value_type> result  = 0;
+
+      auto my_fut_result = 
+      impl->m_executor.bulk_twoway_execute([functor=m_functor,offset=m_policy.begin()](auto i, std::atomic<value_type>* result, auto) {
+        value_type val = 0;
+        ParallelReduce::_invoke(functor, offset+i, val, std::is_same<work_tag_t, void>{});
+        *result += val;
+      }, n, [&]()-> std::atomic<value_type>* {return &result;}, []() {return 0;} )
+    ;
+
+    auto result2 = my_fut_result.get();
+    m_result_ptr[0] = result;
+  }
+};
 
 } // end namespace Impl
 } // end namespace Kokkos
