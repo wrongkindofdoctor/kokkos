5ac00546ae7bcac5527ed40807cc058b13566857
jeffmiles63 <jsmiles@apollo.sandia.gov>
2019-09-17 10:44:05 -0600

    update get_debug_value() so that it will compile with std < 14 also add additional template for types with size < 4

--- core/src/impl/Kokkos_ViewMapping.hpp
+++ core/src/impl/Kokkos_ViewMapping.hpp
@@ -2669,18 +2669,28 @@ struct ViewValueFunctor<ExecSpace, ValueType, true /* is_scalar */> {
   void* verify_result;
 
   template <class T>
-  KOKKOS_INLINE_FUNCTION static constexpr
-      typename Kokkos::Impl::enable_if<sizeof(T) == sizeof(int), T>::type
+  KOKKOS_INLINE_FUNCTION static 
+      typename std::enable_if<sizeof(T) == sizeof(int), T>::type
       get_debug_value() {
     return (T)0xFEFEFEFE;
   }
 
   template <class T>
-  KOKKOS_INLINE_FUNCTION static constexpr typename Kokkos::Impl::enable_if<
+  KOKKOS_INLINE_FUNCTION static typename std::enable_if<
       sizeof(T) != sizeof(int) && sizeof(T) == sizeof(unsigned long long int),
       T>::type
   get_debug_value() {
     return (T)0xFEFEFEFEFEFEFEFE;
+  }
+
+  template <class T>
+  KOKKOS_INLINE_FUNCTION static typename std::enable_if<
+      (sizeof(T) < sizeof(int)),T>::type
+  get_debug_value() {
+    if (sizeof(T) == 2)
+      return (T)0xFEFE;
+    else
+      return (T)0xFE;
   }
 
   KOKKOS_INLINE_FUNCTION
